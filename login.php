659fe248aaf2f050386dbe4c9f59d4d79affdf1f
Asifur Rahman <90130314+AsifurRahman10@users.noreply.github.com>
2021-11-08 01:35:30 +0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="login.css"/>
+    </head>
+
+    <body>
+        <div class="full_body">
+            <form method="POST" action="loginValidation.php">
+                <div class="login">
+
+                    <div class="login_text">
+                        LOGIN
+                    </div>
+
+                    <div class="input_info">
+                        <div class="label">
+                            User ID
+                        </div>
+                        <div class="input_box">
+                            <input type="text" name="user_id">
+                        </div>
+                    </div>
+
+                    <div class="input_info">
+                        <div class="label">
+                            Password
+                        </div>
+                        <div class="input_box">
+                            <input type="text" name="password">
+                        </div>
+                    </div>
+
+                    <div class="input_info">
+                        <div class="label">
+
+                        </div>
+                        <div class="input_box">
+                            <input type="submit" value="Login" name="submit">
+                        </div>
+                    </div>
+
+                    <div class="input_info">
+                        <div class="label">
+
+                        </div>
+                        <div class="input_box">
+                        <input type="button" value="Register">
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
